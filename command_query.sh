4d80412ca0d3a8e9479d92eb1e4f269da955d3ac
MartinGraves <mjg40@cam.ac.uk>
2023-01-18 19:07:44 +0000

    Added read so the cmd window does not  automatically close

--- command_query.sh
+++ command_query.sh
@@ -1,3 +1,4 @@
 curl --request GET "https://cuhimaging-dicom-ms.dicom.azurehealthcareapis.com/v1/changefeed/v1/studies?StudyInstanceUID=1.2.826.0.1.3680043.8.498.13230779778012324449356534479549187420" \
 --header "Accept: application/dicom+json" \
 --header "Authorization: Bearer $1"
+read
